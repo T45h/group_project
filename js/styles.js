dda6a28b9870a9f5c8f339fb935f70d29687cd75
Harriet Gillett <harrietzannagillett@gmail.com>
2019-04-05 00:32:37 +0100

    i cant make a galleryyy

--- js/styles.js
+++ js/styles.js
@@ -1,3 +1,8 @@
 window.PixleeAsyncInit = function()
     {Pixlee.init({apiKey:'OJdmiQCo9vY6u1yHlcl9'});
     Pixlee.addSimpleWidget({widgetId:'17151'});};</script>
+
+    // MDB Lightbox Init
+$(function () {
+$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
