63373dde2da6236ab8f9f6916186a6308b91a821
Tash <ncg@live.co.uk>
2019-04-06 03:43:40 +0100

    Nav bar developement
    made sticky and transparent. attempted javascript to alter active nav bar item as you scroll

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+window.PixleeAsyncInit = function()
+    {Pixlee.init({apiKey:'OJdmiQCo9vY6u1yHlcl9'});
+    Pixlee.addSimpleWidget({widgetId:'17151'});};</script>
+
+    // MDB Lightbox Init
+$(function () {
+$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+//nav bar scrolling attempts
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#menu-center a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#menu-center ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
