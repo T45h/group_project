447a865b3dee49c8e2fed1a1fa422c3e3126f771
Harriet Gillett <harrietzannagillett@gmail.com>
2019-04-08 23:19:57 +0100

    text changes, deleting unnecessary javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,3 @@
 window.PixleeAsyncInit = function()
     {Pixlee.init({apiKey:'OJdmiQCo9vY6u1yHlcl9'});
     Pixlee.addSimpleWidget({widgetId:'17151'});};</script>
-
-    // MDB Lightbox Init
-$(function () {
-$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-});
-
-
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
-//nav bar scrolling attempts
-
-f
-}
-
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
